67f5e57dab25b9688cefa977688ac2bfe61eac1b
Matthew Yerkes <mattyerkes23@gmail.com>
2023-01-10 18:52:29 -0500

    create qb percentile charts and and helper functions

--- data.py
+++ data.py
@@ -12,6 +12,40 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+'''
+Helper functions
+'''
+def create_radar_chart(ax, angles, player_data, color='blue'):
+    
+    # Plot data and fill with team color
+    ax.plot(angles, np.append(player_data[-(len(angles)-1):], 
+            player_data[-(len(angles)-1)]), color=color, linewidth=2)
+    ax.fill(angles, np.append(player_data[-(len(angles)-1):], 
+            player_data[-(len(angles)-1)]), color=color, alpha=0.2)
+    
+    # Set category labels
+    ax.set_xticks(angles[:-1])
+    ax.set_xticklabels(categories)
+    
+    # Remove radial labels
+    ax.set_yticklabels([])
+
+    # Add player name
+    ax.text(np.pi/2, 1.7, player_data[0], ha='center', va='center', 
+            size=18, color=color)
+    
+    # Use white grid
+    ax.grid(color='white', linewidth=1.5)
+
+    # Set axis limits
+    ax.set(xlim=(0, 2*np.pi), ylim=(0, 1))
+
+    return ax
+
+# Function to get QB data
+def get_qb_data(data, team):
+  return np.asarray(data[data['Tm'] == team])[0]
+
 # URL of page
 url = 'https://www.pro-football-reference.com/years/2022/passing.htm'
 # Open URL and pass to BeautifulSoup
@@ -38,3 +72,77 @@ data = pd.DataFrame(qb_stats, columns=column_headers[1:])
 new_columns = data.columns.values
 new_columns[-6] = 'Yds_Sack'
 data.columns = new_columns
+
+# Select stat categories
+categories = ['Cmp%', 'Yds', 'TD', 'Int', 'Y/A', 'Rate']
+
+# Create data subset for radar chart
+data_radar = data[['Player', 'Tm'] + categories]
+
+# Check data types (would return object type since scraped)
+# data_radar.dtypes
+
+# Convert data to numerical values
+data_radar[categories] = data[categories].apply(pd.to_numeric)
+
+# Remove ornamental characters for achievements
+data_radar['Player'] = data_radar['Player'].str.replace('*', '')
+data_radar['Player'] = data_radar['Player'].str.replace('+', '')
+
+# Filter by passing yards
+data_radar_filtered = data_radar[data_radar['Yds'] > 1500]
+
+# Create columns with percentile rank
+for i in categories:
+  data_radar_filtered[i + '_Rank'] = data_radar_filtered[i].rank(pct=True)
+
+# We need to flip the rank for interceptions
+data_radar_filtered['Int_Rank'] = 1 - data_radar_filtered['Int_Rank']
+
+# General plot parameters
+mpl.rcParams['font.size'] = 16
+mpl.rcParams['axes.linewidth'] = 0
+mpl.rcParams['xtick.major.pad'] = 15
+
+team_colors = {'ARI':'#97233f', 'ATL':'#a71930', 'BAL':'#241773', 'BUF':'#00338d', 'CAR':'#0085ca', 'CHI':'#0b162a', 'CIN':'#fb4f14', 'CLE':'#311d00', 'DAL':'#041e42', 'DEN':'#002244', 'DET':'#0076b6', 'GNB':'#203731', 'HOU':'#03202f', 'IND':'#002c5f', 'JAX':'#006778', 'KAN':'#e31837', 'LAC':'#002a5e', 'LAR':'#003594', 'MIA':'#008e97', 'MIN':'#4f2683', 'NWE':'#002244', 'NOR':'#d3bc8d', 'NYG':'#0b2265', 'NYJ':'#125740', 'OAK':'#000000', 'PHI':'#004c54', 'PIT':'#ffb612', 'SFO':'#aa0000', 'SEA':'#002244', 'TAM':'#d50a0a', 'TEN':'#0c2340', 'WAS':'#773141'}
+
+# Calculate angles for radar chart
+offset = np.pi/6
+angles = np.linspace(0, 2*np.pi, len(categories) + 1) + offset
+
+# Create figure
+fig = plt.figure(figsize=(8, 8), facecolor='white')
+# Add subplots
+ax1 = fig.add_subplot(221, projection='polar', facecolor='#ededed')
+ax2 = fig.add_subplot(222, projection='polar', facecolor='#ededed')
+ax3 = fig.add_subplot(223, projection='polar', facecolor='#ededed')
+ax4 = fig.add_subplot(224, projection='polar', facecolor='#ededed')
+# Adjust space between subplots
+plt.subplots_adjust(hspace=0.8, wspace=0.5)
+# Get QB data
+sf_data = get_qb_data(data_radar_filtered, 'SFO')
+sea_data = get_qb_data(data_radar_filtered, 'SEA')
+ari_data = get_qb_data(data_radar_filtered, 'ARI')
+lar_data = get_qb_data(data_radar_filtered, 'LAR')
+# Plot QB data
+ax1 = create_radar_chart(ax1, angles, lar_data, team_colors['LAR'])
+ax2 = create_radar_chart(ax2, angles, ari_data, team_colors['ARI'])
+ax3 = create_radar_chart(ax3, angles, sea_data, team_colors['SEA'])
+ax4 = create_radar_chart(ax4, angles, sf_data, team_colors['SFO'])
+plt.show()
+
+# MVP Race
+# Create figure
+fig = plt.figure(figsize=(8, 4), facecolor='white')
+# Add subplots
+ax1 = fig.add_subplot(121, projection='polar', facecolor='#ededed')
+ax2 = fig.add_subplot(122, projection='polar', facecolor='#ededed')
+# Adjust space between subplots
+plt.subplots_adjust(hspace=0.8, wspace=0.5)
+# Get QB data
+KAN_data = get_qb_data(data_radar_filtered, 'KAN')
+BUF_data = get_qb_data(data_radar_filtered, 'BUF')
+# Plot QB data
+ax1 = create_radar_chart(ax1, angles, BUF_data, team_colors['BUF'])
+ax2 = create_radar_chart(ax2, angles, KAN_data, team_colors['KAN'])
+plt.show()
